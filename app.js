d6d798706f894ee4bb6f8b6fc6b4d03541f3fc2a
DouglasMV <douglasmv@ymail.com>
2019-11-24 15:58:52 -0300

    menu anima

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ btnMenu.addEventListener('click', function (e) {
   span1.classList.toggle('span1')
   span2.classList.toggle('span2')
   span3.classList.toggle('span3')
-  menu.classList.toggle('display-none')
+  //menu.classList.toggle('display-none')
+  menu.style.height === '100vh' ? menu.style.height = '0vh' : menu.style.height = '100vh'
 })
 
 menu.addEventListener('click', function (e) {
   span1.classList.remove('span1')
   span2.classList.remove('span2')
   span3.classList.remove('span3')
-  menu.classList.add('display-none')
+  //menu.classList.add('display-none')
+  menu.style.height = '0vh'
 })
